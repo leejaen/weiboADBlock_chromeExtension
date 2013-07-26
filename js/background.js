3f079239dc97a3f6898668a36008c45335a7d11a
leejaen <leejaen@gmail.com>
2013-07-26 01:47:59 +0100

    first commit

---
+++ js/background.js
@@ -0,0 +1,281 @@
+﻿(function ()
+{
+    chrome.browserAction.setIcon({ path: "img/icon0.png" });//切换图标
+    setTimeout(function ()
+    {
+        var hasWB = false;
+        chrome.tabs.getSelected(function (selectedTab)
+        {
+            // Retrieves all tabs
+            chrome.tabs.getAllInWindow(function (tabs)
+            {
+                for (var i = 0; i < tabs.length; i++)
+                {
+                    // Excludes selected and loaded tabs
+                    if (tabs[i].id !== selectedTab.id && tabs[i].url.lastIndexOf("weibo.com"))
+                    {
+                        hasWB = true;
+                        break;
+                    }
+                }
+            });
+        });
+
+        if (hasWB == true)
+        {
+            chrome.browserAction.setIcon({ path: "img/icon.png" });//切换图标
+        }
+    }, 10000);
+})();
+var updateWBADIcon = function (number, STATE)
+{
+    var html = "";
+    if (number == 0)
+    {
+        number = "";
+    }
+    if (STATE)
+    {//hidden
+        chrome.browserAction.setIcon({ path: "img/icon0.png" });//切换图标
+    } else
+    {
+        chrome.browserAction.setIcon({ path: "img/icon.png" });//切换图标
+    }
+    chrome.browserAction.setBadgeText({ text: String(number) });//提示透明度文字
+}
+
+chrome.extension.onRequest.addListener(function (request, sender, sendResponse)
+{//返回请求数据
+    //console.log("state="+state);
+    //console.log(request.method + "      " + request.number);
+    if (request.method == "setWBADicon")
+    {
+        //console.log("STATE="+STATE);
+        var counter = request.number;
+        var STATE = request.STATE;
+        updateWBADIcon(counter, STATE);
+        sendResponse({});
+    }
+    if (request.method == "getWBADsmurf")
+    {
+        var smurfs = "$这条微博没有节操,推广来自,微博推广,来自粉丝头条,精彩微博推荐,";//这种肯定是广告
+        var htmlKey = "taobao.com,sinaapp.com,wby.so,url.com,.feitao,emop.cn,pianke.me,tmall.com,taourl.com,51fanli.com,immomo.com,kktalk.com,renrentuan.,zhubajie.com,taoppp.com,weiqc.com,xigouq00.com,wqc.so,likeface.com,event.video.sina.com,event.weibo.com,apps.weibo.com,badge.weibo.com,ua.yesweibo.com,wyht.so,dlj.so,qoo10.cn,weiligongshe.com,fanlibang.com,h2w.iask.cn,acs.56.com,lcpinai.com,lijitui.com,popular_buss,W_no_border,";//默认2013年1月16日17:28:25删除kan.weibo.com,2013年2月17日9:44:37删除vdisk.weibo.comW_no_border,W_no_border:好友关注
+        var sectionKey = "#pl_content_biztips,div[ad-data],*[node-type^='ad'],.promotion_twist,div[feedtype='ad'],";//默认顶部（发布框下方）广告,下部广告(a[suda-uatrack],),v3.6一样
+        var keyWord = "", exKeyword = "", others = "", Box_right = "";//, photostyle = "";
+        for (var i = 0; i < window.localStorage.length; i++)
+        {
+            var key = window.localStorage.key(i);
+            var value = window.localStorage.getItem(key);
+
+            switch (key)
+            {
+                //自定义关键字
+                case "keyword":
+                    keyWord = value;
+                    break;
+                case "exkeyword":
+                    exKeyword = value;
+                    break;
+                    //微博广告
+                case "awardRetweet"://有奖转发
+                    smurfs += "$抽奖的、转发的都是骗子,中奖,奖地址,大奖,仅售,精美礼,奖品,礼品,赢多项奖品,就有机会,有机会获得,幸运粉丝,有奖转发,转发并,的请转发,内转发,同意的转,转发有奖,转发本微博,转发此微博,评论本微博,@3位,@5位,@3名,@5名,好礼,";
+                    break;
+                case "recommendedFollowing"://推荐关注
+                    smurfs += "$推荐关注←_←,关注@,推荐@,快来@,终于有人开通,推荐关注,轻松关注,推荐给大家,必须关注,强烈推荐,关注下,喜欢这个微博,喜欢这个帐号,也关注TA,";
+                    break;
+                case "promotionalAD"://促销广告
+                    smurfs += "$促销广告,地址：t.cn,周年庆,UGG,爆促销,包邮,卖疯了,详情请点击,全网最低,假一赔,可享受,可免费,免费获,数量有限,可以享受,除痘,正品,到货啦,痘痘,发售,还不赶紧,折起售,优惠！,1折,2折,3折,5折,9折,猪油膏,祛痘,起拍,小肚腩,去黑头,贵宾专线,贵宾热线,";
+                    break;
+                case "micromsg"://微信推广
+                    smurfs += "$微信推广,微信里也有,微信号,扫描二维码,";
+                    break;
+                case "groupon"://团购秒杀
+                    smurfs += "$团购秒杀,折秒杀,元秒杀,团购地址,分享地址,秒杀地址,推荐地址,购买地址,抢购地址,宝贝地址,活动链接,抢货地址,";
+                    break;
+                case "rebate"://返利网推广
+                    smurfs += "$返利网推广,返利网,省下更多,";
+                    break;
+                case "invalidWB"://无效微博
+                    smurfs += "$无效微博,此微博已被作者删除,此微博已被删除,此微博不适宜对外公开,";
+                    break;
+                    //其他设置
+                case "exSelf":
+                    others += value + ",";
+                    break;
+                    //case "photostyle":
+                    //    photostyle += value + ",";
+                    //    break;
+                    ////其他设置
+                    //case "moreList":
+                    //    moreList += value + ",";
+                    //    break;
+                    //板块广告
+                case "feed_list_recommend"://屏蔽精彩内容推荐微博
+                    sectionKey += ".WB_feed div[node-type='feed_list_recommend'],";
+                    break;
+                case "pl_content_top"://屏蔽上册导航条内容
+                    sectionKey += "#pl_content_top,";
+                    break;
+                case "WB_webim"://屏蔽右侧私信聊天
+                    sectionKey += "#WB_webim,#cometd_uc,.webim_list&.WBIM_fold,";
+                    break;
+                case "interest"://可能感兴趣的人
+                    sectionKey += "#trustPagelet_indexright_recom>div:eq(1)>div:eq(1),#trustPagelet_recom_interestv5,#trustPagelete_recom_interest";
+                    break;
+                case "zone"://会员专区会员动态
+                    sectionKey += "#trustPagelet_recom_memberv5,#trustPagelet_member_zone,";
+                    break;
+                case "allinone"://会员专区会员动态
+                    sectionKey += "#trustPagelet_recom_allinonev5,#trustPagelete_recom_allinone,";
+                    break;
+                case "noticeboard"://公告栏
+                    sectionKey += "#pl_rightmod_noticeboard,";
+                    break;
+                case "checkin"://签到
+                    sectionKey += "#trustPagelet_checkin_lotteryv5,#trustPagelet_checkin_lottery,";
+                    break;
+                case "invite"://邀请
+                    sectionKey += "#trustPagelet_checkin_lotteryv5,#trustPagelet_ugrowth_invite,";
+                    break;
+                case "mood"://心情
+                    sectionKey += "#pl_content_mood,";
+                    break;
+                case "medal"://勋章
+                    sectionKey += "#pl_rightmod_medal,";
+                    break;
+                case "oldUser"://旧版微博精选
+                    sectionKey += "#pl_guide_oldUser,";
+                    break;
+                case "app"://最近使用应用列表
+                    sectionKey += "#pl_leftnav_app,";
+                    break;
+                case "ads_"://最近使用应用列表
+                    sectionKey += "li[id^='ads'],";
+                    break;
+                case "ads"://最近使用应用列表
+                    sectionKey += "#pl_rightmod_ads36,#pl_rightmod_ads35,";
+                    break;
+                case "help"://玩转微博/帮助
+                    sectionKey += "#pl_rightmod_help,";
+                    break;
+                    //case "Box_right"://微博详细内容页右侧所有内容
+                    //    Box_right += "#Box_right,";
+                    //    break;
+                case "moduleMyRelation"://我的关注和粉丝
+                    sectionKey += "#pl_profile_moduleMyRelation,";
+                    break;
+                case "pl_profile_giftBox"://我的礼物盒
+                    sectionKey += "#pl_profile_giftBox,.W_layer&.layer_guide,.function_guide&.function_s,.function_guide&.function_h,";
+                    break;
+                case "pl_profile_hisGiftBox"://他的礼物盒
+                    sectionKey += "#pl_profile_hisGiftBox,.W_layer&.layer_guide,.function_guide&.function_s,.function_guide&.function_h,";
+                    break;
+                case "pl_profile_unfollow"://不是自己关注用户信息推荐
+                    sectionKey += "#pl_profile_unfollow,";
+                    break;
+                case "trustPagelet_profile_openApplist"://开启应用卡片 记录你的互联网生活
+                    sectionKey += "#trustPagelet_profile_openApplist,";
+                    break;
+                case "modulealbum"://个人页面微关系
+                    sectionKey += "#pl_profile_modulealbum,";
+                    break;
+                case "moduleHisRelation"://个人页面微相册
+                    sectionKey += "#pl_profile_moduleHisRelation,";
+                    break;
+                case "recomendedkan"://微博详细内容页推荐微刊
+                    sectionKey += "#trustPagelet_mblog_weikan,";
+                    break;
+                case "hotmblog"://微博详细内容页热门微博
+                    sectionKey += "#trustPagelet_mblog_hotmblog,";
+                    break;
+                case "weiba"://微博详细内容页推荐微吧
+                    sectionKey += "#trustPagelet_indexright_recom>div:eq(1)>div:eq(2),#trustPagelet_mblog_weiba,";
+                    break;
+                case "photo"://微博详细内容页推荐图片
+                    sectionKey += "#trustPagelet_mblog_photo,";
+                    break;
+                case "music"://微博详细内容页推荐音乐
+                    sectionKey += "#trustPagelet_mblog_music,";
+                    break;
+                case "recommblog"://微博详细内容页推荐微博
+                    sectionKey += "#pl_rightmod_recommblog,";
+                    break;
+                case "topic"://微博详细内容页推荐话题
+                    sectionKey += "#trustPagelet_mblog_topic,";
+                    break;
+                case "recomperson"://微博详细内容页推荐关注
+                    sectionKey += "#pl_rightmod_recomperson,";
+                    break;
+                case "pl_content_commentTopNav"://热评微博
+                    sectionKey += "#pl_content_commentTopNav,";
+                    break;
+                case "pl_rightmod_feedback"://新浪微博意见反馈
+                    sectionKey += "#pl_rightmod_feedback,";
+                    break;
+                case "pl_rightmod_helpcommonbox"://共同评论使用小帮助
+                    sectionKey += "#pl_rightmod_helpcommonbox,";
+                    break;
+                case "pl_rightmod_reportentry"://微博举报处理中心
+                    sectionKey += "#pl_rightmod_reportentry,";
+                    break;
+                case "pl_rightmod_helpat"://@使用小帮助
+                    sectionKey += "#pl_rightmod_helpat,";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (keyWord.lastIndexOf(",") == keyWord.length - 1)
+        {
+            keyWord = keyWord.substring(0, keyWord.length - 1);
+        }
+        sendResponse({
+            keyword: keyWord//可屏蔽关键字
+            , exkeyword: exKeyword//排除关键字
+            , htmlKey: htmlKey//链接广告
+            , smurf: smurfs.substring(0, smurfs.length - 1)//页面广告
+            , others: others.substring(0, others.length - 1)//其他设置，排除自己
+            , photostyle: window.localStorage["photostyle"]//形象设置
+            , sectionKey: sectionKey.substring(0, sectionKey.length - 1)//板块广告
+            , colorvalue: window.localStorage["colorvalue"]//颜色设置
+            , showOrDelWBADInfo: window.localStorage["showOrDelWBADInfo"]//显示屏蔽微博设置
+            , moreList: window.localStorage["moreList"]//自动展开左侧所有分组
+            , Box_right: window.localStorage["Box_right"]//显示屏蔽微博设置(已关注微博所有的列表)
+            , superScrolling: window.localStorage["superScrolling"]//超级滚动
+            , superGallery: window.localStorage["superGallery"]//微博画廊
+            , noforward: window.localStorage["noforward"]//同时转发到我的微博
+        });
+    }
+});
+
+chrome.tabs.onRemoved.addListener(function ()
+{
+    chrome.browserAction.setIcon({ path: "img/icon0.png" });//切换图标
+    chrome.browserAction.setBadgeText({ text: String("") });//提示透明度文字
+});//添加监听事件
+chrome.tabs.onCreated.addListener(function ()
+{
+    var hasWB = false;
+    chrome.tabs.getSelected(function (selectedTab)
+    {
+        // Retrieves all tabs
+        chrome.tabs.getAllInWindow(function (tabs)
+        {
+            for (var i = 0; i < tabs.length; i++)
+            {
+                // Excludes selected and loaded tabs
+                if (tabs[i].id !== selectedTab.id && tabs[i].url.lastIndexOf("weibo.com"))
+                {
+                    hasWB = true;
+                    break;
+                }
+            }
+        });
+    });
+
+    if (hasWB == true)
+    {
+        chrome.browserAction.setIcon({ path: "img/icon.png" });//切换图标
+    }
+});//添加监听事件
---
+++ js/settings.js
@@ -0,0 +1,238 @@
+﻿$(document).bind("pageinit", function ()
+{
+    if (window.localStorage.getItem("usedWBADtag") != "USED")
+    {
+        //用户没有用过本插件使用默认配置
+        init();
+    }
+    function init(resource)
+    {
+        window.localStorage.clear();
+        if (resource == "btnDefault")
+        {//不清空usedWBADtag
+            window.localStorage["usedWBADtag"] = "USED";//记录下用户已经使用过
+        };
+        window.localStorage["ads"] = "ads";
+        window.localStorage["ads_"] = "ads_";
+        window.localStorage["allinone"] = "allinone";
+        window.localStorage["app"] = "app";
+        window.localStorage["awardRetweet"] = "awardRetweet";
+        window.localStorage["checkin"] = "checkin";
+        window.localStorage["colorvalue"] = "#ff0097";
+        window.localStorage["exSelf"] = "exSelf";
+        window.localStorage["exwbadawardRetweet"] = "exwbadawardRetweet";
+        window.localStorage["feed_list_recommend"] = "feed_list_recommend";
+        window.localStorage["groupon"] = "groupon";
+        window.localStorage["help"] = "help";
+        window.localStorage["hotmblog"] = "hotmblog";
+        window.localStorage["interest"] = "interest";
+        window.localStorage["invalidWB"] = "invalidWB";
+        window.localStorage["invite"] = "invite";
+        window.localStorage["keyword"] = "$星座,星座,白羊座,金牛座,双子座,巨蟹,狮子座,处女座,天秤,天蝎,射手座,摩羯,水瓶座,双鱼座,$SB广告,姐妹们,鸡皮肤,$广告大主臭不要脸,上海煜宸,梦享家,欧尚坊,shopbop,大众点评客户端,谷卡名车广场,im8官网微博,折企业网站微博,上海麦丹,竹叶青茶,八客网,嫁伊,八阵电商,返利网-海淘返利,麦开网,沪江网外语学习,橡果国际旗舰店,返利网粉丝论坛,591结婚网,返利网淘宝返利,柏菲娜,捷贝网络,史泰博中国,优娅化妆品销售部,爱上淘创意,";
+        window.localStorage["exkeyword"] = "流星雨";
+        window.localStorage["medal"] = "medal";
+        window.localStorage["micromsg"] = "micromsg";
+        window.localStorage["mood"] = "mood";
+        window.localStorage["moreList"] = "moreList";
+        window.localStorage["music"] = "music";
+        window.localStorage["oldUser"] = "oldUser";
+        window.localStorage["photo"] = "photo";
+        window.localStorage["photostyle"] = "photo_default";
+        window.localStorage["pl_profile_giftBox"] = "pl_profile_giftBox";
+        window.localStorage["pl_profile_hisGiftBox"] = "pl_profile_hisGiftBox";
+        window.localStorage["pl_profile_unfollow"] = "pl_profile_unfollow";
+        window.localStorage["promotionalAD"] = "promotionalAD";
+        window.localStorage["rebate"] = "rebate";
+        window.localStorage["recomendedkan"] = "recomendedkan";
+        window.localStorage["recommblog"] = "recommblog";
+        window.localStorage["recommendedFollowing"] = "recommendedFollowing";
+        window.localStorage["recomperson"] = "recomperson";
+        window.localStorage["showOrDelWBADInfo"] = "deleteWBAD";
+        //window.localStorage["superScrolling"] = "superScrolling";
+        //window.localStorage["superGallery"] = "superGallery";
+        window.localStorage["noforward"] = "noforward";
+        window.localStorage["topic"] = "topic";
+        window.localStorage["trustPagelet_profile_openApplist"] = "trustPagelet_profile_openApplist";
+        window.localStorage["weiba"] = "weiba";
+        window.localStorage["zone"] = "zone";
+    }
+    function loadProfile()
+    {//遍历选定配置中已选定的项目
+        $("input").attr("checked", false).checkboxradio("refresh");
+        $("input").checkboxradio('enable').checkboxradio("refresh");
+        $("textarea").val("");
+
+        var colortag = 0, deletetag = 0, styletag = 0;
+        for (var i = 0; i < window.localStorage.length; i++)
+        {
+            var key = window.localStorage.key(i);
+            var value = window.localStorage.getItem(key);
+            console.log("value=" + value);
+            if (key == "Box_right")
+            {
+                if (value == "Box_right")
+                {//勾选本项，其他项目失效
+                    $("#mainWb,#myWb,#xxxxWb,#oldver").find("input[type='checkbox']").checkboxradio('disable').checkboxradio("refresh");
+                    $("#Box_right").checkboxradio('enable').attr("checked", true).checkboxradio("refresh");
+                } else
+                {
+                    $("#mainWb,#myWb,#xxxxWb,#oldver").find("input[type='checkbox']").checkboxradio('enable').checkboxradio("refresh");
+                    $("#Box_right").checkboxradio('disable').attr("checked", false).checkboxradio("refresh");
+                }
+                continue;
+            }
+            //if (key == "WB_webim")
+            //{
+            //    if (value == "WB_webim")
+            //    {
+            //        $("#WB_webim").checkboxradio('enable').attr("checked", true).checkboxradio("refresh");
+            //    } else
+            //    {
+            //        $("#WB_webim").checkboxradio('disable').attr("checked", false).checkboxradio("refresh");
+            //    }
+            //    continue;
+            //}
+            if (key == "keyword")
+            {
+                $("#keyword").val(value);
+                continue;
+            }
+            if (key == "exkeyword")
+            {
+                $("#exkeyword").val(value);
+                continue;
+            }
+            if (key == "showOrDelWBADInfo")
+            {
+                deletetag = 1;
+                $("#infosett input[type='radio'][value='" + value + "']").attr("checked", true).checkboxradio("refresh");
+                if (value == "showWBAD")
+                {
+                    $("#collsett input[type='radio']").checkboxradio('enable').checkboxradio("refresh");
+                } else if (value == "deleteWBAD")
+                {
+                    $("#collsett input[type='radio']").checkboxradio('disable').checkboxradio("refresh");
+                }
+                continue;
+            }
+            if (key == "photostyle")
+            {
+                styletag = 1;
+                $("#photo_style input[type='radio'][value='" + value + "']").attr("checked", true).checkboxradio("refresh");
+                continue;
+            }
+            if (key == "colorvalue")
+            {
+                colortag = 1;
+                $("#collsett input[type='radio'][value='" + value + "']").attr("checked", true).checkboxradio("refresh");
+                continue;
+            }
+            $("#" + value).attr("checked", true).checkboxradio("refresh");
+        }
+        if (deletetag == 0)
+        {//没有勾选，默认显示
+            window.localStorage["showOrDelWBADInfo"] = "showWBAD";
+            $("input[type='radio'][value='showWBAD']").attr("checked", true).checkboxradio("refresh");
+        }
+        if (colortag == 0)
+        {//没有勾选，默认颜色
+            window.localStorage["colorvalue"] = "#00aba9";
+            $("input[type='radio'][value='#00aba9']").attr("checked", true).checkboxradio("refresh");
+        }
+        if (styletag == 0)
+        {//没有勾选，默认颜色
+            window.localStorage["photostyle"] = "photo_default";
+            $("input[type='radio'][value='photo_default']").attr("checked", true).checkboxradio("refresh");
+        }
+    }
+    loadProfile();
+    $("input[type='checkbox']").bind("click", function (event, ui)
+    {
+        var id = $(this).attr("id");
+        if ($(this).attr("checked") == "checked")
+        {
+            window.localStorage[id] = id;
+            //alert("id=" + window.localStorage[id]);
+        } else
+        {
+            window.localStorage.removeItem(id);
+        }
+        window.localStorage["usedWBADtag"] = "USED";//记录下用户已经使用过
+    });
+    $("#btnDefault").bind("click", function (event, ui)
+    {
+        init("btnDefault");
+        loadProfile();
+        history.back();
+    });
+    $("#Box_right").bind("click", function (event, ui)
+    {
+        if ($(this).attr("checked") == "checked")
+        {//勾选本项，其他项目失效
+            window.localStorage["Box_right"] = "Box_right";
+            $("#mainWb,#myWb,#xxxxWb,#oldver").find("input[type='checkbox']").checkboxradio('disable').checkboxradio("refresh");
+            $("#Box_right").attr("checked", true).checkboxradio("refresh");
+        } else
+        {
+            window.localStorage.removeItem("Box_right");
+            $("#mainWb,#myWb,#xxxxWb,#oldver").find("input[type='checkbox']").checkboxradio('enable').checkboxradio("refresh");
+            $("#Box_right").attr("checked", false).checkboxradio("refresh");
+        }
+        $("#Box_right").checkboxradio('enable').checkboxradio("refresh");
+        window.localStorage["usedWBADtag"] = "USED";//记录下用户已经使用过
+    });
+    $("#photo_style input[type='radio']").bind("click", function (event, ui)
+    {
+        var stylevalue = $(this).attr("value");
+        if ($(this).attr("checked") == "checked")
+        {
+            window.localStorage["photostyle"] = stylevalue;
+        } else
+        {
+            window.localStorage.removeItem(stylevalue);
+        }
+        window.localStorage["usedWBADtag"] = "USED";//记录下用户已经使用过
+    });
+    $("#collsett input[type='radio']").bind("click", function (event, ui)
+    {
+        var colorvalue = $(this).attr("value");
+        if ($(this).attr("checked") == "checked")
+        {
+            window.localStorage["colorvalue"] = colorvalue;
+        }
+        else
+        {
+            window.localStorage.removeItem(colorvalue);
+        }
+        window.localStorage["usedWBADtag"] = "USED";//记录下用户已经使用过
+    });
+    $("#infosett input[type='radio']").bind("click", function (event, ui)
+    {
+        var colorvalue = $(this).attr("value");
+        if ($(this).attr("checked") == "checked")
+        {
+            window.localStorage["showOrDelWBADInfo"] = colorvalue;
+        } else
+        {
+            window.localStorage.removeItem(colorvalue);
+        }
+
+        if (colorvalue == "showWBAD")
+        {
+            $("#collsett input[type='radio']").checkboxradio('enable').checkboxradio("refresh");
+        } else if (colorvalue == "deleteWBAD")
+        {
+            $("#collsett input[type='radio']").checkboxradio('disable').checkboxradio("refresh");
+        }
+        window.localStorage["usedWBADtag"] = "USED";//记录下用户已经使用过
+        //选中：$("input[type='checkbox']").attr("checked", true).checkboxradio("refresh");
+        //不选：$("input[type='checkbox']").attr("checked", false).checkboxradio("refresh");
+    });
+    $("textarea").bind("blur", function (event, ui)
+    {
+        var id = $(this).attr("id");
+        var val = $(this).val();
+        window.localStorage[id] = val;
+        window.localStorage["usedWBADtag"] = "USED";//记录下用户已经使用过
+    });
+});
